906de93277dee5c326de15d405349d2e29254da7
parsariyahi <pany.parsariyahi@gmail.com>
2024-01-30 10:35:25 +0330

    ``Day 2`` add sol.py

--- Day 2/sol.py
+++ Day 2/sol.py
@@ -1,6 +1,39 @@
+MAX_RED_CUBES = 12
+MAX_GREEN_CUBES = 13
+MAX_BLUE_CUBSE = 14
+
+RULES = {
+    "red": MAX_RED_CUBES,
+    "green": MAX_GREEN_CUBES,
+    "blue": MAX_BLUE_CUBSE,
+}
+
 with open("Day 2\input.txt", "r") as file:
     data = file.read()
-    data = data.splitlines()
+    games = data.splitlines()
     total = 0
 
-    print(data)
+    for game in games:
+        game_info = game.split(":")
+        game_id = game_info[0].split(" ")[1]
+        rounds = game_info[1].split(";")
+        possible = False
+        
+        for round_ in rounds:
+            cubes = round_.split(",")
+            for cube in cubes:
+                cube_info = cube.split(" ")
+                cube_count = cube_info[1]
+                cube_color = cube_info[2]
+                rule = RULES.get(cube_color)
+                if int(cube_count) > rule:
+                    break
+            else:
+                continue
+
+            break
+
+        else:
+            total += int(game_id)
+
+print(total)
